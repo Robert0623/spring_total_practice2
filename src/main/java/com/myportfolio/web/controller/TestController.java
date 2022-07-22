858245b70dbfd47710ff0a1b9906f0b9ddcd1c24
Robert0623 <tone8943@gmail.com>
2022-07-22 23:48:31 +0900

    쓰기, 수정

--- src/main/java/com/myportfolio/web/controller/TestController.java
+++ src/main/java/com/myportfolio/web/controller/TestController.java
@@ -1,17 +1,28 @@
 package com.myportfolio.web.controller;
 
+import com.myportfolio.web.domain.BoardDto;
+import com.myportfolio.web.domain.PageHandler;
 import com.myportfolio.web.domain.User;
+import com.myportfolio.web.service.BoardService;
+import com.myportfolio.web.service.UserService;
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.beans.propertyeditors.CustomMapEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.net.URLEncoder;
+import java.util.*;
 
 
 public class TestController {
