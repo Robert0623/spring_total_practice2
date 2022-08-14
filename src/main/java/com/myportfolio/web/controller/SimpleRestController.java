2c5d3104b23e5a63300e48bbbe65552b8400c82d
HyeonWooSeo <tone8943@gmail.com>
2022-08-14 20:42:58 +0900

    comment practice

--- src/main/java/com/myportfolio/web/controller/SimpleRestController.java
+++ src/main/java/com/myportfolio/web/controller/SimpleRestController.java
@@ -2,15 +2,12 @@ package com.myportfolio.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SimpleRestController {
     @GetMapping("/test")
     public String test() {
-        return "test"; //뷰이름
+        return "test_old"; //뷰이름
     }
 
 }
